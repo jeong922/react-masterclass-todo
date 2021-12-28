bf984f21de4c393807f3ce788e960a005bd2522b
jeong922 <hwajeong922@naver.com>
2021-12-28 17:56:39 +0900

    commit

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#353b48",
+  textColor: "white",
+  accentColor: "#FF9F86",
+  listbg: "#62708D",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "black",
+  accentColor: "#FF9F86",
+  listbg: "white",
+};
